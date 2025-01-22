7d67288ac3980e97277032de6419b33d26b6a7e4
techguru8237 <techguru8237@gmail.com>
2025-01-23 00:27:40 +0900

    Switch db connection

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,7 @@ app.use(errorHandler);
 
 // Create the /check-server route
 app.get("/check-server", (req, res) => {
+  console.log("Server is running!");
   res.status(200).send("Server is running");
 });
 
